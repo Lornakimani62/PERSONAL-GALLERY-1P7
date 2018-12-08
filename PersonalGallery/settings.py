96ce985d0da31aaeacfced71aa6ccaec17b6d34d
lorna nashipae <kimanilorna@yahoo.com>
2018-12-08 19:11:56 +0300

    Adds the application models and the database for which migrations are created

--- PersonalGallery/settings.py
+++ PersonalGallery/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'PersonalGallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'gallery',
+        'NAME':'gallerys',
         'USER':'lorna',
         'PASSWORD':'0724276722',
     }
--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_category(self):
+        self.update()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self):
+        self.update()
+
+
+    def __str__(self):
+        return self.name
+
+class Image(models.Model):
+    name = models.CharField(max_length=30)
+    Description = models.TextField()
+    location = models.ForeignKey('Location')
+    category = models.ManyToManyField('Category')
+
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    def update_image(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_images(cls):
+        images = cls.objects.all()
+        return images
+
+    def __str__(self):
+        return self.name
+
+
