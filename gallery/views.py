ed2bfe96837c016360725a90cd9c53b91604d939
lorna nashipae <kimanilorna@yahoo.com>
2018-12-09 10:23:59 +0300

    Adds the methods to the view function

--- gallery/views.py
+++ gallery/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
+from .models import Location,Category,Image
 
 # Create your views here.
 def home(request):
-
+    images = Image.get_all_images()
     return render(request,'index.html')
 
 def image(request):
