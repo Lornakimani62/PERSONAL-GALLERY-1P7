48e50a029546bb8e81ade46d36d92b07a2a6772d
lorna nashipae <kimanilorna@yahoo.com>
2018-12-09 16:01:29 +0300

    Adds view functions to the application

--- gallery/views.py
+++ gallery/views.py
@@ -4,11 +4,17 @@ from .models import Location,Category,Image
 # Create your views here.
 def home(request):
     images = Image.get_all_images()
-    return render(request,'index.html')
+    title = 'Gallery Home'
+    return render(request,'index.html',{'title':title, 'images':image})
 
 def image(request):
 
-    return render(request,'details.html')
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+
+    return render(request,'details.html',{"image":image})
 
 def search(request):
 
